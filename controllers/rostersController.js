edd3ab5b4bed71dc3e4d41b7db9a2c3c6cd7ec83
Matt H <44037139+Mbwata@users.noreply.github.com>
2021-09-19 12:45:24 -0500

    copied in latest updates

--- controllers/rostersController.js
+++ controllers/rostersController.js
@@ -17,7 +17,7 @@ exports.index = async (req, res) => {
 
 exports.create = async (req, res) => {
   const players = await Players.find({})
-    .select('id name position')
+    .select('_id name position squadNumber')
     .sort('name');
   res.render("rosters/create", {
     title: "Create Roster",
@@ -56,7 +56,7 @@ exports.show = async (req, res) => {
 exports.edit = async (req, res) => {
   const rosterPromise = Roster.findById(req.params.id);
   const playersPromise = Players.find({})
-    .select('_id name position')
+    .select('_id name position squadNumber')
     .sort('name');
 
   const [roster, players] = await Promise.all([rosterPromise, playersPromise]);
@@ -93,10 +93,60 @@ exports.update = async (req, res) => {
       context: "query"
     }
   );
+  const defaultRostersCount = await Roster.find({
+      default: true
+  }).countDocuments()
+  if (defaultRostersCount == 0) {
+      updates.default = true;
+      await Roster.findOneAndUpdate(
+          {
+              _id: req.params.id
+          },
+          {
+              $set: updates
+          },
+          {
+              new: true,
+              runValidators: true,
+              context: "query"
+          }
+      )
+      req.flash("info", `${roster.rosterTitle} was set to default because there were no others set to default`);
+  }
   DELETE_ROSTERS_CACHE();
   req.flash("success", `${roster.rosterTitle} has been updated!`);
   res.redirect(`/rosters/${roster._id}`);
 };
+
+exports.duplicate = async (req, res) => {
+	const rosterPromise = Roster.findById(req.params.id);
+	const playersPromise = Players.find({})
+		.select('_id name position')
+		.sort('name')
+
+	const [roster, players] = await Promise.all([rosterPromise, playersPromise])
+
+	res.render("rosters/duplicate", {
+		title: `Duplicate ${roster.rosterTitle}`,
+		roster,
+		players,
+	})
+}
+
+exports.duplicateSave = async (req, res) => {
+	let values = {
+		rosterTitle: req.body.rosterTitle,
+		season: req.body.season,
+		players: req.body.players ? req.body.players.map((pl) => JSON.parse(pl)) : [],
+		active: req.body.active ? true : false,
+		default: req.body.default ? true : false
+	}
+
+	const roster = await new Roster(values).save()
+	DELETE_ROSTERS_CACHE()
+	req.flash("success", `${roster.rosterTitle} was created from a duplicate.`)
+	return res.redirect(`/rosters/${roster.id}`)
+}
 
 exports.deleteConfirm = async (req, res) => {
   const roster = await Roster.findById(req.params.id);
